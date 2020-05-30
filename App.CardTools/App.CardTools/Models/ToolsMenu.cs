0da6b2af3039fa089b9323fd4ff99c3fdfd5cc09
Alexandre Sanlim <alexandreslima@outlook.com>
2020-05-30 11:52:02 -0300

    add home page

---
+++ App.CardTools/App.CardTools/Models/ToolsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App.CardTools.Models
+{
+    public class ToolsMenu : MenuItem
+    {
+        public ToolsMenu()
+        {
+            Child = new List<ToolsMenu>();
+        }
+
+        public Color PrimaryColor { get; set; }
+
+        public Color OnPrimaryColor { get; set; }
+
+        public Color SecondaryColor { get; set; }
+
+        public Color OnSecondaryColor { get; set; }
+
+        public string Icon { get; set; }
+
+        public List<ToolsMenu> Child { get; private set; }
+
+        public ToolsMenu AddChild(List<ToolsMenu> childrens)
+        {
+            //childrens.SetColorInMenuList();
+
+            //var back = new MenuBody
+            //{
+            //    Text = "Back",
+            //    Icon = IconFont.AngleLeft,
+            //    BackgroundColor = Color.FromHex("#bdc3c7")
+            //};
+
+            //childrens.Insert(0, back);
+
+            this.Child = childrens;
+
+            return this;
+        }
+    }
+}
--- App.CardTools/App.CardTools/ViewModels/HomeViewModel.cs
+++ App.CardTools/App.CardTools/ViewModels/HomeViewModel.cs
@@ -1,10 +1,75 @@
-﻿using System;
+﻿using App.CardTools.Models;
+using App.CardTools.Models.Icon;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace App.CardTools.ViewModels
 {
     public class HomeViewModel : BaseViewModel
     {
+        public HomeViewModel()
+        {
+            LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+            LoadItemsCommand.Execute(null);
+        }
+
+        async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                ToolsList = new ObservableCollection<ToolsMenu>();
+                ToolsList.Clear();
+                var items = new List<ToolsMenu>()
+                {
+                    new ToolsMenu
+                    {
+                         Text = "Connection",
+                         PrimaryColor = Color.FromHex("#4ac7cb"),
+                         OnPrimaryColor = Color.FromHex("#FFFFFF"),
+                         SecondaryColor = Color.FromHex("#6b1230"),
+                         OnSecondaryColor = Color.FromHex("#FFFFFF"),
+                         Icon = FontAwesomeSolid.Wifi
+                    },
+                    new ToolsMenu
+                    {
+                         Text = "Connection"
+                    },
+                    new ToolsMenu
+                    {
+                         Text = "Connection"
+                    }
+                };
+
+                //await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    ToolsList.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public Command LoadItemsCommand { get; set; }
+
+        private ObservableCollection<ToolsMenu> _toolsList;
+        public ObservableCollection<ToolsMenu> ToolsList
+        {
+            set { SetProperty(ref _toolsList, value); }
+            get { return _toolsList; }
+        }
     }
 }
