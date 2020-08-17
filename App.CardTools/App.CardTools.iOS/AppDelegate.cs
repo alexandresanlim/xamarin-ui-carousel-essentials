83e9f92aed118b8585bab392acf20709543f357a
Alexandre Sanlim <alexandreslima@outlook.com>
2020-08-17 17:55:21 -0300

    remove old

--- App.CardTools/App.CardTools.iOS/AppDelegate.cs
+++ App.CardTools/App.CardTools.iOS/AppDelegate.cs
@@ -24,7 +24,7 @@ namespace App.CardTools.iOS
         {
             //global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             global::Xamarin.Forms.Forms.Init();
-            global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
+            //global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
